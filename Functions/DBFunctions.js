01219a79c586fd4629975a457fde0c6d40ae3987
hackathon IDF <hack@cs.colman.ac.il>
2021-06-09 11:35:07 +0300

    different project now

---
+++ Functions/DBFunctions.js
@@ -0,0 +1,44 @@
+exports.createTable = createTable = (client, tableName, cols) => {
+    const query = `
+    CREATE TABLE ron_table (
+        firstName varchar,
+        lastName varchar,
+        age int
+        );
+        `;
+
+    let r = client.connect((err) => console.log(err))
+    client.query(query, (err) => {
+        if (err) {
+            console.error("Creating DB Table Error -> " + err);
+        } else {
+            console.error("Table Created Successfully")
+            client.end();
+        }
+    });
+}
+
+exports.connectDB = connectDB = async (client) => {
+    const query = `
+    SELECT * FROM ${dbConfig.dbName}`
+    client.connect()
+    let result = await client.query(query, (err) => console.log("ERROR -> " + err));
+    client.end()
+    return result
+}
+
+exports.getTablesNames = this.getTablesNames = async (client) => {
+    const query = `select * from public.users` //`SELECT table_name FROM information_schema.tables
+                  //    WHERE table_schema='public'`
+    let r = client.connect((err) => console.log(err))
+    let result = await client.query(query, (err) => {
+        if(err) {
+            console.log("ERROR -> " + err);
+            return "ERROR"
+        }
+        else {
+            client.end()
+        }
+    })
+    return result
+}
---
+++ test.js
@@ -0,0 +1,64 @@
+const { Client } = require('pg');
+const express = require('express');
+const cors = require('cors')
+const app = express();
+const DBFunctions = require('./Functions/DBFunctions.js')
+var client = null
+
+app.use(cors())
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.header("Access-Control-Allow-Headers", "x-access-token, Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.get('/', (req, res) => {
+    res.send("Empty (But working) Web Page")
+});
+
+app.post('/connect', (req, res) => {
+    console.log("connect -> " + req.body.dbName)
+    client = new Client({
+        user: 'ron',
+        host: '172.30.92.221',//req.body.ip,
+        database: 'demodb',//req.body.dbName,
+        password: 'Bsmch@500K!',
+        port: 5432
+    });
+    res.header( "Access-Control-Allow-Origin" );
+    res.send(true)
+})
+
+app.get('/create', (req, res) => {
+    //console.log("create -> " + req.body.dbName)
+    //DBFunctions.createTable(client, req.body.tableName, req.body.cols)
+    client = new Client({
+        user: 'ron',
+        host: '172.30.92.221',//req.body.ip,
+        database: 'demodb',//req.body.dbName,
+        password: 'Bsmch@500K!',
+        port: 5432
+    });
+    DBFunctions.createTable(client, "", "")
+    res.header( "Access-Control-Allow-Origin" );
+    res.send("Table created Successfully")
+})
+
+app.post('/tables', async (req, res) => {
+    client = new Client({
+        user: 'ron',
+        host: '172.30.92.221',//req.body.ip,
+        database: 'demodb',//req.body.dbName,
+        password: 'Bsmch@500K!',
+        port: 5432
+    });
+    let result = await DBFunctions.getTablesNames(client)
+    res.send(result)
+})
+
+const server = app.listen(8080, () => {
+    console.log(`Express running → PORT ${server.address().port}`);
+});
